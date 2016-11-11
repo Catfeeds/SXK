6765ec8efe3ddcc1a261cd048e6cb5d38dd84451
青叶mac1 <dongzheng.dd@163.com>
2016-11-11 14:27:40 +0800

    SXK测试

---
+++ SXK/app/src/main/java/com/example/cfwifine/sxk/Section/MineNC/testAC.java
@@ -0,0 +1,15 @@
+package com.example.cfwifine.sxk.Section.MineNC;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cfwifine.sxk.R;
+
+public class testAC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_ac);
+    }
+}
